cf1b8f6a309b5057cfce2781b750c8b223376e47
sthitiacharya <sthitiacharya@gmail.com>
2021-05-14 21:30:58 +0800

    Issue #2: User Registration RESTful web service method

--- ChallengeTrackerApplication/ChallengeTrackerApplication-war/src/java/ws/rest/UserResource.java
+++ ChallengeTrackerApplication/ChallengeTrackerApplication-war/src/java/ws/rest/UserResource.java
@@ -6,7 +6,6 @@
 package ws.rest;
 
 import ejb.session.stateless.UserEntitySessionBeanLocal;
-import entity.Program;
 import entity.User;
 import java.util.List;
 import java.util.logging.Level;
@@ -15,6 +14,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
@@ -23,6 +23,9 @@ import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import util.exception.InputDataValidationException;
+import util.exception.UnknownPersistenceException;
+import util.exception.UserUsernameExistException;
 
 /**
  * REST Web Service
@@ -44,6 +47,24 @@ public class UserResource {
      */
     public UserResource() {
         //userSessionBeanLocal = lookupUserEntitySessionBeanLocal();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createUser(User newUser) {
+        if (newUser != null) {
+            try {
+                Long userEntityId = userEntitySessionBeanLocal.createNewUser(newUser);
+                return Response.status(Response.Status.OK).entity(userEntityId).build();
+            } catch (UserUsernameExistException | UnknownPersistenceException ex) {
+                return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+            } catch (InputDataValidationException ex) {
+                return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
+            }
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Invalid create new user request").build();
+        }
     }
 
     @Path("retrieveAllUsers")
