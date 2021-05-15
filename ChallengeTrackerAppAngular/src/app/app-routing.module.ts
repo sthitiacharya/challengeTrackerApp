dc471df10846d03bf61c3b82a4e740fd9590d583
sthitiacharya <sthitiacharya@gmail.com>
2021-05-15 23:18:17 +0800

    Issue #3: User Login Angular Component HTML markup

--- ChallengeTrackerAppAngular/src/app/app-routing.module.ts
+++ ChallengeTrackerAppAngular/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateNewProgramComponent } from "./program/create-new-program/create-new-program.component";
 import { CreateNewMilestoneComponent } from "./milestone/create-new-milestone/create-new-milestone.component";
 import { RegisterUserComponent } from "./userManagement/register-user/register-user.component";
-
+import { UserLoginComponent } from "./userManagement/user-login/user-login.component";
 const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
   { path: 'program/createProgram', component: CreateNewProgramComponent },
   { path: 'milestone/createMilestone', component: CreateNewMilestoneComponent },
-  { path: 'register', component: RegisterUserComponent }
+  { path: 'register', component: RegisterUserComponent },
+  { path: 'index', component: UserLoginComponent }
 ];
 
 @NgModule({
