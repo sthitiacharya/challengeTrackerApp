07679b3fdb40b2821ac249e64a1b54c3156696a4
sthitiacharya <sthitiacharya@gmail.com>
2021-05-15 22:40:38 +0800

    Issue #3: User Login RESTful Web Service Method

--- ChallengeTrackerApplication/ChallengeTrackerApplication-war/src/java/ws/rest/UserResource.java
+++ ChallengeTrackerApplication/ChallengeTrackerApplication-war/src/java/ws/rest/UserResource.java
@@ -19,11 +19,13 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import util.exception.InputDataValidationException;
+import util.exception.InvalidLoginCredentialException;
 import util.exception.UnknownPersistenceException;
 import util.exception.UserUsernameExistException;
 
@@ -65,6 +67,28 @@ public class UserResource {
         } else {
             return Response.status(Response.Status.BAD_REQUEST).entity("Invalid create new user request").build();
         }
+    }
+    
+    @Path("userLogin")
+    @GET
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response userLogin(@QueryParam("username") String username,
+            @QueryParam("password") String password) {
+        try {
+            User userEntity = userEntitySessionBeanLocal.userLogin(username, password);
+            userEntity.getEnrolledPrograms().clear();
+            userEntity.getMilestoneList().clear();
+            userEntity.getMilestonesCreated().clear();
+            userEntity.getProgramsManaging().clear();
+            System.out.println("********** UserResource.userLogin(): User " + userEntity.getUsername() + " login remotely via web service");           
+            return Response.status(Status.OK).entity(userEntity).build();
+        } catch (InvalidLoginCredentialException ex) {
+            return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+
     }
 
     @Path("retrieveAllUsers")
