a2287636d771becf5c7f13d528a40bb3da036362
sthitiacharya <sthitiacharya@gmail.com>
2021-05-24 00:28:15 +0800

    Pull Request #8: modifications to UI of web application

--- ChallengeTrackerAppAngular/src/app/app.component.ts
+++ ChallengeTrackerAppAngular/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from '../app/services/session.service';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,8 @@ import { SessionService } from '../app/services/session.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild(MatSidenav)
+ sidenav!: MatSidenav;
   title = 'ChallengeTrackerAppAngular';
   constructor(private router: Router,
     public sessionService: SessionService) { }
