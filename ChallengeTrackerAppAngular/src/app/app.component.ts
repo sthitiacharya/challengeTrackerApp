1a3ad333761ee9ef9a7cb16246e1c275fc65e4f8
sthitiacharya <sthitiacharya@gmail.com>
2021-06-17 10:37:03 +0800

    Pull Request #18: updated code for delete milestone

--- ChallengeTrackerAppAngular/src/app/app.component.ts
+++ ChallengeTrackerAppAngular/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { SessionService } from '../app/services/session.service';
 import { MatSidenav } from '@angular/material/sidenav';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,14 @@ export class AppComponent {
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
   title = 'ChallengeTrackerAppAngular';
+  user : User
   constructor(private router: Router,
-    public sessionService: SessionService) { }
+    public sessionService: SessionService) {
+      this.user = new User();
+     }
 
   ngOnInit(): void {
+    this.user = this.sessionService.getCurrentUser();
   }
 
   userLogout(): void {
